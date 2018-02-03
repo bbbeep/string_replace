7630eba5d9dcbd544c1815a75874ff40e9358e51
David Gray <david.ian.gray@gmail.com>
2018-02-03 13:59:14 -0500

    Slight changes to replace.c

--- replace.c
+++ replace.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "replace.h"
+#include <string.h>
 //should contain main()
+
+int main(int argc, char **argv)
+{
+	printf("argv: %s\n\n",argv[0]);	
+	char *foo = getopt(argc, argv);
+
+	printf("\n %d \n",foo);	
+	
+	return 0;
+}
+
+
+char *getopt(int argc, char **argv)
+{
+	printf("argc: %d", argc);	
+	if(argc > 1)
+	{
+		return 0;	
+		//return argv[1];
+	} else {
+		return 1;	
+		fprintf(0, "No command line arguments provided");
+		// return "";
+	}
+}
