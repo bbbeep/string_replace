ef4e7de037e90a0f5f3f29d5dcb2878e68f22606
David Gray <david.ian.gray@gmail.com>
2018-02-04 20:39:00 -0500

    cleaning up traversal.c

--- traversal.c
+++ traversal.c
@@ -6,32 +6,6 @@
 #include "traversal.h"
 #include "replace.h"
 
-//function to return fname lead by its relative location.
-char *proper_filename(char *current, char *fname) {
-	//char *s = NULL;
-
-	if(strcmp(current, ".")){	
-		//current is not "."		
-		char c[sizeof(current) + sizeof(fname) + 2];
-		strcpy(c, current);
-
-		strcat(c, "/");
-		strcat(c, fname);
-		return c;
-	} else {
-		char d[sizeof(fname) + 1];
-		strcpy(d, fname);
-		return d;
-	}
-
-	/*if(s == NULL){
-		fprintf(stderr, "filenaming error");
-	}
-	
-	printf("From current: %s and fname: %s, returning: %s\n", current, fname, s);
-	return s;
-	*/
-}
 
 void traverse(char *current) 
 {
@@ -59,14 +33,8 @@ void traverse(char *current)
 				if(dp->d_name[0] != '.'){
 					// make a string of the right size
 					// This line isn't problematic right? allocating more space than I may need for this string....	
-					char recurse_into[sizeof(current)+sizeof(dp->d_name)+2];
-					if(strcmp(current, ".")){	
-						strcpy(recurse_into, current);
-						strcat(recurse_into, "/");
-						strcat(recurse_into, dp->d_name);
-					} else {
-						strcpy(recurse_into, dp->d_name);
-					}
+					char recurse_into[strlen(current)+strlen(dp->d_name)+2];
+					sprintf(recurse_into, "%s/%s", current, dp->d_name);
 					printf("descending into... %s\n", recurse_into);
 					traverse(recurse_into);
 				}
@@ -76,11 +44,10 @@ void traverse(char *current)
 				
 				if(strstr(dp->d_name, ".txt")!=NULL) {
 					if(strcmp(current, ".")){	
-						printf("found %s/%s\n", current, dp->d_name);	
-						//add_fname_to_fcount_array(current/dp->d_name);	
-					} else {
-						printf("found %s\n", dp->d_name);	
-						add_fname_to_fcount_array(dp->d_name, 0);
+						char file_loc[strlen(current)+strlen(dp->d_name)+2];
+						sprintf(file_loc, "%s/%s", current, dp->d_name);
+						printf("found %s\n", file_loc);	
+						add_fname_to_fcount_array(file_loc, 0);	
 					}	
 				}
 			} else {
