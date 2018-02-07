2166ba4d18782f9f0950d38d33c5d1accd0afc22
David Gray <david.ian.gray@gmail.com>
2018-02-07 10:01:32 -0500

    I forgot to add two headerfiles!

---
+++ report.h
@@ -0,0 +1,16 @@
+/**
+ * @file replace.c
+ * @author David Gray
+ * Student ID: 40055149
+ * @date 6 Feb 2018
+ *
+ * Header file for report.c
+ */ 
+
+
+#ifndef REPORT_H_ 
+#define REPORT_H_
+
+void make_report(char *target, struct file_count *farray); 
+
+#endif /* REPORT_H_ */
---
+++ text.h
@@ -0,0 +1,20 @@
+/**
+ * @file text.h
+ * @author David Gray
+ * Student ID: 40055149
+ * @date 6 Feb 2018
+ *
+ * Header file for text.c
+ */ 
+
+
+#ifndef TEXT_H_
+#define TEXT_H_
+
+int replace(char *fname, char *target);
+
+void s_to_lower(char *s);
+void s_to_uppwer(char *s);
+
+#endif /* TEXT_H_ */
+
